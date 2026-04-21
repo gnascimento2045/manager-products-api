bf95c36d81f26c3e60b11a310adf33b61aa3448a
GabrielPC <kira.msd10@gmail.com>
2026-04-21 15:04:51 -0300

    adicionado swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Manager Products API')
+    .setDescription('API for product and category management')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
 }
+
 bootstrap();
